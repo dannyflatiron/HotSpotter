7240ed5848f5d151bc103148d5233880c85d263e
dannyflatiron <dannyflatiron@gmail.com>
2020-09-03 20:08:00 -0400

    dropped database to troubleshoot user and review model relationship

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_200406) do
+ActiveRecord::Schema.define(version: 2020_09_03_234748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_09_03_200406) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "reviews", "users"
 end
