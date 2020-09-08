993ab500e471dacb046294fb11cc632045f0381b
dannyflatiron <dannyflatiron@gmail.com>
2020-09-08 12:48:11 -0400

    moved controller files to api/v1 directory to preserve intended file structure

---
+++ backend/app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::ReviewsController < ApplicationController
+    before_action :set_review
+    
+    def index
+        reviews = Review.all
+
+        render json: reviews
+    end
+
+    def show
+        render json: review
+    end
+
+    def create
+        review = Review.new(review_params)
+
+        if review.save
+            render json: review
+        else
+            render json: review.errors
+        end
+    end
+
+    def update
+        if review.update(review_params)
+            render json: review
+        else
+            render json: review.errors
+        end
+    end
+
+    def destroy
+        review.destroy
+    end
+
+
+
+    private
+
+    def set_review
+        review = Review.find_by(params[:id])
+    end
+
+    def review_params
+        params.require(:review).permit(:content, :user_id)
+    end
+end
