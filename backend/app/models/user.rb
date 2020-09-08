60a85d92e766137e21aa4ca815a9e3e123641160
dannyflatiron <dannyflatiron@gmail.com>
2020-09-08 14:09:19 -0400

    validate username presence, uniqueness, and length

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-
     has_many :reviews
     has_many :favorited_locations
+
+    validates :username, :presence => true, :uniqueness => true, :length => { :minimum => 4 }
 end
