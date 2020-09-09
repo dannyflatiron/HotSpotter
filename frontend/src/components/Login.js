fe4f8929d15f0430478bdc51e4b3e0611698d965
dannyflatiron <dannyflatiron@gmail.com>
2020-09-09 13:58:59 -0400

    created basic form, connected store and loginForm action to pass formData to the action to the reducer

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,13 +1,37 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { updateLoginForm } from '../actions/users/loginForm.js'
+import { login } from '../actions/users/currentUser.js'
 
-const Login = () => {
+const Login = ({loginFormData, updateLoginForm, login}) => {
+
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        const formData = {
+            ...loginFormData,
+            [name]: value
+        }
+        updateLoginForm(formData)
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        login(loginFormData)
+    }
+
     return (
-        <form onSubmit={}>
-            <input type="text" onChange={} value={} name={username}/>
-            <input type="text" onChange={} value={} name={password}/>
-            <input type="submit" value="Log In"/>
-        </form>
+    <form onSubmit={handleSubmit}>
+        <input placeholder="username" value={loginFormData.username} name="username" type="text" onChange={handleInputChange} />
+        <input placeholder="password" value={loginFormData.password} name="password" type="text" onChange={handleInputChange} />
+        <input type="submit" value="Log In"/>
+    </form>
     )
 }
 
-export default Login
+const mapStateToProps = state => {
+    return {
+        loginFormData: state.loginForm
+    }
+}
+
+export default connect(mapStateToProps, { updateLoginForm, login })(Login)
