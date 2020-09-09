541189522388fa1f10adb9fef78708df11fdd155
dannyflatiron <dannyflatiron@gmail.com>
2020-09-09 14:00:11 -0400

    fixed typo

--- frontend/src/reducers/loginForm.js
+++ frontend/src/reducers/loginForm.js
@@ -1,11 +1,11 @@
 const initialState = {
     username: "",
-    password: ''
+    password: ""
 }
 
 export default (state = initialState, action) => {
     switch(action.type) {
-        case "UPDATE_LOGIN_FROM":
+        case "UPDATE_LOGIN_FORM":
             return action.formData
         default:
             return state
