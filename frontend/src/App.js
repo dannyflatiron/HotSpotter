6b1211e893420dbcae76db4000c5064c57fb06e4
dannyflatiron <dannyflatiron@gmail.com>
2020-09-09 00:32:31 -0400

    removed componentDidMount

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,6 @@ import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
 
 class App extends React.Component {
 
-
-  componentDidMount() {
-    fetch("http://localhost:3000/api/v1/users")
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }
-
   render() {
     return (
       <div className="App">
