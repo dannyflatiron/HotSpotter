57094b2eae074c4d5cdde5562ffe96c308e63227
dannyflatiron <dannyflatiron@gmail.com>
2020-09-09 03:30:28 -0400

    building redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import usersReducer from './reducers/usersReducer'
 
-ReactDOM.render(    
-<App />,
+const reducer = combineReducers({
+    usersReducer
+})
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>,
 document.getElementById('root')
 );
 
