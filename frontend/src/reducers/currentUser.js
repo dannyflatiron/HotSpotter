a5093c424b63dd97eb7aea64df2b4f5323d81e4b
dannyflatiron <dannyflatiron@gmail.com>
2020-09-09 04:38:23 -0400

    matched case with type from action currentUser

--- frontend/src/reducers/currentUser.js
+++ frontend/src/reducers/currentUser.js
@@ -1,5 +1,7 @@
 export default (state = [], action) => {
     switch(action.type) {
+        case "SET_CURRENT_USER":
+            return action.user
         default:
             return state
     }
