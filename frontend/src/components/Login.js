fde67cdff7bcf9576aa6f53feca62d1ebada47a0
dannyflatiron <dannyflatiron@gmail.com>
2020-09-09 11:54:47 -0400

    created login form presentational component, a login form reducer and action

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <form onSubmit={}>
+            <input type="text" onChange={} value={} name={username}/>
+            <input type="text" onChange={} value={} name={password}/>
+            <input type="submit" value="Log In"/>
+        </form>
+    )
+}
+
+export default Login
