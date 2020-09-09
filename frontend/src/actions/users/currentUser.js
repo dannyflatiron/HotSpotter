ee8327d36232259f934b4e2971a7794625395be7
dannyflatiron <dannyflatiron@gmail.com>
2020-09-09 15:22:48 -0400

    use dispatch to setCurrentUser. created fetch request GET for current user

--- frontend/src/actions/users/currentUser.js
+++ frontend/src/actions/users/currentUser.js
@@ -15,5 +15,34 @@ export const login = loginFormData => {
             },
             body: JSON.stringify(loginFormData)
         })
+        .then(response => response.json())
+        .then(user => {
+            if (user.errors) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+        .catch(console.log)
+    }
+}
+
+export const getCurrentUser = () => {
+    return dispatch => {
+        return fetch("http://localhost:3000/api/v1/get_current_user", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (response.error) {
+                alert(response.error)
+            } else {
+                dispatch(setCurrentUser(response.data))
+            }
+        })
+        .catch(console.log)
     }
 }
