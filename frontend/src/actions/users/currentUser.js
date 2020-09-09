0a5f4b58cd1d4080476b5124f35dea018fe3abd7
dannyflatiron <dannyflatiron@gmail.com>
2020-09-09 13:57:00 -0400

    fixed type that prevented submitted form data from reaching the store

--- frontend/src/actions/users/currentUser.js
+++ frontend/src/actions/users/currentUser.js
@@ -6,13 +6,14 @@ export const setCurrentUser = user => {
 }
 
 export const login = credentials => {
+    console.log("credentials", credentials)
     return dispatch => {
-        return fetch("http://localhost:3000/api/v1/login"), {
+        return fetch("http://localhost:3000/api/v1/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(credentials)
-        }
+        })
     }
 }
