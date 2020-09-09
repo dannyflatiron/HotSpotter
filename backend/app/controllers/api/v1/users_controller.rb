c0cc980cf2a7dc5b3518f9b6a5e55cac7e0da90a
dannyflatiron <dannyflatiron@gmail.com>
2020-09-09 00:15:03 -0400

    added instance variables to properly render users/id endpoint

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -10,7 +10,8 @@ class Api::V1::UsersController < ApplicationController
 
   # GET /users/1
   def show
-    user_json = UserSerializer.new(user).serialized_json
+    # binding.pry
+    user_json = UserSerializer.new(@user).serialized_json
     render json: user_json
   end
 
@@ -27,22 +28,22 @@ class Api::V1::UsersController < ApplicationController
 
   # PATCH/PUT /users/1
   def update
-    if user.update(user_params)
-      render json: user
+    if @user.update(user_params)
+      render json: @user
     else
-      render json: user.errors, status: :unprocessable_entity
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /users/1
   def destroy
-    user.destroy
+    @user.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      user = User.find(params[:id])
+      @user = User.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
