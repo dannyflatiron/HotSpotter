2dbb1e72889778ec22830941836f37f1dbe5245f
dannyflatiron <dannyflatiron@gmail.com>
2020-09-10 18:09:21 -0400

    created signup form

---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { updateSignupForm } from '../actions/users/signupForm.js'
+import { signup } from '../actions/users/currentUser.js'
+import UsernameInput from './UsernameInput.js'
+import PasswordInput from './PasswordInput.js'
+
+const Signup = ({signupFormData, updateSignupForm}) => {
+
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        const formData = {
+            ...signupFormData,
+            [name]: value
+        }
+        updateSignupForm(formData)
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        // signup(signupFormData)
+    }
+
+    return (
+    <form onSubmit={handleSubmit}>
+        <UsernameInput placeholder="username" value={signupFormData.username} type="text" name="username" onChange={handleInputChange} />
+        <PasswordInput placeholder="password" value={signupFormData.password} type="text" name="password" onChange={handleInputChange} />
+        <input type="submit" value="Sign Up"/>
+    </form>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        signupFormData: state.signupForm
+    }
+}
+
+export default connect(mapStateToProps, { updateSignupForm })(Signup)
