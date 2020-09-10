db4ed8ce5b0efb82ed6e83c4d563ea59c2bf9218
dannyflatiron <dannyflatiron@gmail.com>
2020-09-10 18:02:45 -0400

    commented Nav and Map components to view signup component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import '../App.css';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
 import { connect } from 'react-redux'
 import { getCurrentUser } from "../actions/users/currentUser.js"
 import NavBar from "./NavBar.js"
 import Review from "./Review.js"
-import '../App.css';
+import Login from "./Login.js"
+import Signup from "./Signup.js"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
 class App extends React.Component {
@@ -16,9 +19,14 @@ class App extends React.Component {
   render() {
     return (
     <div className="App">
-      <NavBar />
-      <Map google={this.props.google} />
+      {/* <NavBar /> */}
+      {/* <Map google={this.props.google} /> */}
       <Review />
+      <Router >
+        <Route exact path='/signup' component={Signup}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/reviews' component={Review}/>
+      </Router>
     </div>  
 
     )
