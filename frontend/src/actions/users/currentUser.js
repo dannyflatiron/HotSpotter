fb1114071a8fa15220701e1aa53256c7351d9872
dannyflatiron <dannyflatiron@gmail.com>
2020-09-09 21:32:31 -0400

    developing action creator for logout feature

--- frontend/src/actions/users/currentUser.js
+++ frontend/src/actions/users/currentUser.js
@@ -28,6 +28,23 @@ export const login = loginFormData => {
     }
 }
 
+export const logout = () => {
+    return (dispatch) => {
+        dispatch(clearCurrentUser)
+        return fetch("http://localhost:3000/api/v1/logout", {
+            credentials: "include",
+            method: "DELETE",
+            }
+        )
+    }
+}
+
+export const clearCurrentUser = () => {
+    return {
+        type: "CLEAR_CURRENT_USER"
+    }
+}
+
 export const getCurrentUser = () => {
     return dispatch => {
         return fetch("http://localhost:3000/api/v1/get_current_user", {
