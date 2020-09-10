494adfc56fb3479f9133b360e9e5892228d587c7
dannyflatiron <dannyflatiron@gmail.com>
2020-09-10 04:29:00 -0400

    seemingly finished action creator that fetches and uses thunk to grab all reviews and pass it to setReviews via dispatch

---
+++ frontend/src/actions/reviews/getReviews.js
@@ -0,0 +1,28 @@
+export const setReviews = reviews => {
+    return {
+        type: "SET_REVIEWS",
+        reviews
+    }
+}
+
+export const getReviews = () => {
+    return dispatch => {
+        return fetch("http://localhost:3000/api//v1/reviews", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then(response => response.json())
+        .then(response => {
+            // if (response.error) {
+            //     alert(response.error)
+            // } else {
+                console.log("getReviews", response.data)
+                dispatch(setReviews(response.data))
+            // }
+        })
+        .catch(console.log)
+    }
+}
