55ce60f2e5bf5ccbf35f391633f4f2ab892e1745
dannyflatiron <dannyflatiron@gmail.com>
2020-09-10 13:49:59 -0400

    fixed content render issue

--- frontend/src/components/ReviewCard.js
+++ frontend/src/components/ReviewCard.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 const ReviewCard = ({ review }) => {
+    console.log(review)
     return (
-        <p>{review.attributues.content}</p>
+        <p>{review.attributes.content}</p>
     )
 }
 
