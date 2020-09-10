714888654b295ff52b2ee51008232afe7d3f600e
dannyflatiron <dannyflatiron@gmail.com>
2020-09-10 01:52:46 -0400

    removed state to place it in Nav Bar component. created NavBar component and almost ready to work on Map component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,38 +1,33 @@
 import React from 'react';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
 import { connect } from 'react-redux'
-import Login from "./Login.js"
-import Logout from "./Logout.js"
 import { getCurrentUser } from "../actions/users/currentUser.js"
+import NavBar from "./NavBar.js"
+import '../App.css';
 
 
 class App extends React.Component {
 
   componentDidMount() {
     this.props.getCurrentUser()
-    // if (this.props.currentUser) {
-    //   this.props.getCurrentUser()
-    // }
   }
 
   render() {
     return (
-    this.props.currentUser ? <Logout/> : <Login/>
+    <div className="App">
+      <NavBar />
+      {/* <Map google={this.props.google} /> */}
+    </div>  
+
     )
   }
 }
 
-const mapStateToProps = ({currentUser}) => {
-  return {
-    currentUser
-  }
-}
-        {/* <Map google={this.props.google} /> */}
 
 
 const WrappedContainer = GoogleApiWrapper({
   // apiKey: (process.env.REACT_APP_API_KEY)
 })(App)
 
-export default connect(mapStateToProps, { getCurrentUser })(WrappedContainer)
+export default connect(null, { getCurrentUser })(WrappedContainer)
  
