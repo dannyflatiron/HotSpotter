a742b31222aaa8bf29a4833b646d6722f20a416a
dannyflatiron <dannyflatiron@gmail.com>
2020-09-10 04:30:42 -0400

    set an initial state variable and added case statement to update store

--- frontend/src/reducers/reviews.js
+++ frontend/src/reducers/reviews.js
@@ -1,6 +1,9 @@
-export default (state = [], action) => {
-    switch (action.type) {
+const initialState = []
 
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case "SET_REVIEWS":
+            return action.reviews
         default:
             return state;
     };
