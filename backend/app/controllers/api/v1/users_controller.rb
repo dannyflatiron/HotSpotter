a7b3ef5132e2f4e7a4dd6bf5c3d2fdd269fe6437
dannyflatiron <dannyflatiron@gmail.com>
2020-09-11 04:18:00 -0400

    wrapped parameters for user must include username, password and password_confirmation. Solved issue of password not being saved

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  wrap_parameters :user, include: [:username, :password, :password_confirmation]
 
   # GET /users
   def index
@@ -17,11 +18,14 @@ class Api::V1::UsersController < ApplicationController
   # POST /users
   def create
     user = User.new(user_params)
-
     if user.save
-      render json: user, status: :created, location: user
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user), status: :created
     else
-      render json: user.errors, status: :unprocessable_entity
+      resp = {
+        error: user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
     end
   end
 
@@ -47,6 +51,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :password_digest)
+      params.require(:user).permit(:username, :password, :password_confirmation)
     end
 end
