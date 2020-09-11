c64049d8b2fa662312674879276c33be403c1a62
dannyflatiron <dannyflatiron@gmail.com>
2020-09-11 03:09:07 -0400

    removed binding.pry

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -10,7 +10,6 @@ class Api::V1::UsersController < ApplicationController
 
   # GET /users/1
   def show
-    # binding.pry
     user_json = UserSerializer.new(@user).serialized_json
     render json: user_json
   end
