02dc20d6a4e70348b0bf387b8e45db623ebda2d6
dannyflatiron <dannyflatiron@gmail.com>
2020-09-11 04:25:04 -0400

    removed attributes since it was null

--- frontend/src/components/WelcomeMessage.js
+++ frontend/src/components/WelcomeMessage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const WelcomeMessage = (props) => {
     return (
-        <p id="logged_in">Welcome, {props.user.attributes.username}</p>
+        <p id="logged_in">Welcome, {props.user.username}</p>
     )
 }
 
