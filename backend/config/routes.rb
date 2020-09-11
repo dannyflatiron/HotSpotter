5871d130f03e179785a856971f6ca3e777ee1ac1
dannyflatiron <dannyflatiron@gmail.com>
2020-09-11 04:18:20 -0400

    added POST route for create user

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   post "/api/v1/login", to: "api/v1/sessions#create"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  post "/api/v1/signup", to: "api/v1/users#create"
   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
 
 
