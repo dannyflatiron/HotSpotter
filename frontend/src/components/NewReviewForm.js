c226be9a69a16d2932b6dffa4a7e7fbed6aba962
dannyflatiron <dannyflatiron@gmail.com>
2020-09-12 15:54:17 -0400

    debugging form data and state work flow. debugging creating new review

--- frontend/src/components/NewReviewForm.js
+++ frontend/src/components/NewReviewForm.js
@@ -1,17 +1,43 @@
 import React from 'react';
+import { updateNewReviewForm } from '../actions/reviews/newReviewForm'
+import { createReview } from '../actions/reviews/createReview.js'
+import { connect } from 'react-redux'
 
+const NewReviewForm = ({newReviewFormData, updateNewReviewForm, createReview, userId}) => {
+// console.log("newReviewFormData", newReviewFormData.content)
 
-const NewReviewForm = () => {
+  const handleChange = event => {
+    let content  = newReviewFormData.content
+    let user = newReviewFormData.user_id
+    content = event.target.value
+    const formData = {
+      content,
+      userId
+    }
 
-  handleChange= () => {
+    updateNewReviewForm(formData)
+    return formData
+  }
 
+  const handleSubmit = (event, userId) => {
+    event.preventDefault()
+    createReview(newReviewFormData, userId)
   }
 
   return (
-    <form>
-      <input name="content" onChange={handleChange} value={"content"}/>
+    <form onSubmit={handleSubmit}>
+      <input name="content" onChange={handleChange} value={undefined} />
+      <input type="submit" value="Submit Review"/>
     </form>
+  )
+};
 
-)};
+const mapStateToProps = (state) => {
+  const userId = state.currentUser ? state.currentUser.id: ""
+  return {
+    newReviewFormData: state.newReviewForm,
+    userId
+  } 
+}
 
-export default NewReviewForm;
+export default connect(mapStateToProps, { updateNewReviewForm, createReview })(NewReviewForm);
