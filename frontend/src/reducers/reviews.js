e53aa8743415f45fd674a235fa6087b0ddd4bac1
dannyflatiron <dannyflatiron@gmail.com>
2020-09-12 15:56:31 -0400

    add new review to initial state of reviews in store

--- frontend/src/reducers/reviews.js
+++ frontend/src/reducers/reviews.js
@@ -1,9 +1,12 @@
 const initialState = []
 
 export default (state = initialState, action) => {
+    console.log("add review action in reducer", action)
     switch (action.type) {
         case "SET_REVIEWS":
             return action.reviews
+        case "ADD_REVIEW":
+            return state.concat(action.review)
         default:
             return state;
     };
