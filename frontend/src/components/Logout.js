1f87e47d3b1d3518b5d3d25b2be82a834c849734
dannyflatiron <dannyflatiron@gmail.com>
2020-09-13 04:18:08 -0400

    not needed anymore. replaced with functionality in NavBar Links

--- frontend/src/components/Logout.js
+++ frontend/src/components/Logout.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { logout } from '../actions/users/currentUser.js'
-
-const Logout = ({ logout }) => {
-
-    return (
-    <form onSubmit={event => {
-        event.preventDefault()
-        logout()
-    }
-    }>
-        <input type="submit" value="Log Out"/>
-    </form>
-    )
-}
+// import React from 'react'
+// import { connect } from 'react-redux'
+// import { logout } from '../actions/users/currentUser.js'
+// import { useHistory } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 
+// const Logout = ({ logout, currentUser }) => {
+//     const history = useHistory()
+//     return (
+//     <form onSubmit={event => {
+//         event.preventDefault()
+//         logout(() => history('/'), currentUser.id)
+//     }
+//     }>
+//         <input type="submit" value="Log Out"/>
+//     </form>
+    
+//     )
+// }
 
-export default connect(null, { logout })(Logout)
+// const mapStateToProps = ({currentUser}) => {
+//     return {
+//         currentUser
+//     }
+// }
+
+// export default connect(mapStateToProps, { logout })(Logout)
