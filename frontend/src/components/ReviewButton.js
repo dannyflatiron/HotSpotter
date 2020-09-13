affcb0fecd6d2a8190cb041efce5354c93be9180
dannyflatiron <dannyflatiron@gmail.com>
2020-09-13 04:18:41 -0400

    not needed anymore. replaced with functionality in NavBar Links

--- frontend/src/components/ReviewButton.js
+++ frontend/src/components/ReviewButton.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { getReviews } from '../actions/reviews/getReviews.js'
+// import React from 'react'
+// import { connect } from 'react-redux'
+// import { getReviews } from '../actions/reviews/getReviews.js'
 
 
-const ReviewButton = () => {
+// const ReviewButton = () => {
 
-    const handleOnClick = event => {
-        event.preventDefault()
-        getReviews()
-    }
+//     const handleOnClick = event => {
+//         event.preventDefault()
+//         getReviews()
+//     }
 
-    return (
-        <button onClick={handleOnClick}>All Reviews</button>
-    )
-}
+//     return (
+//         <button onClick={handleOnClick}>All Reviews</button>
+//     )
+// }
 
-export default connect(null, { getReviews })(ReviewButton)
+// export default connect(null, { getReviews })(ReviewButton)
