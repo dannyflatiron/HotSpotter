587f95dc9c5dcb69dc3c88a77a2ac11d49943bcb
dannyflatiron <dannyflatiron@gmail.com>
2020-09-13 04:28:44 -0400

    removed console log

--- frontend/src/components/WelcomeMessage.js
+++ frontend/src/components/WelcomeMessage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const WelcomeMessage = ({user}) => {
-    console.log("welcome message", user)
     return (
         <p id="logged_in">Welcome, {user.attributes.username}</p>
     )
