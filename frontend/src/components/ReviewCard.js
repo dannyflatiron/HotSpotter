db2ae8ee26f9b1eb025004a125a8aed92ca21357
dannyflatiron <dannyflatiron@gmail.com>
2020-09-13 04:28:11 -0400

    accessing review.content

--- frontend/src/components/ReviewCard.js
+++ frontend/src/components/ReviewCard.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
 const ReviewCard = ({ review }) => {
-    console.log(review)
     return (
-        <p className="review content">{review.attributes.content}</p>
+        <p className="review content">{review.content}</p>
     )
 }
 
