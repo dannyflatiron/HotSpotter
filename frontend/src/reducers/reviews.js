24cd4d57e05f9997efd2ae2f4aefdb2d3be4fc07
dannyflatiron <dannyflatiron@gmail.com>
2020-09-13 04:29:46 -0400

    accessing data at the correct level to update review array state

--- frontend/src/reducers/reviews.js
+++ frontend/src/reducers/reviews.js
@@ -1,12 +1,11 @@
 const initialState = []
 
 export default (state = initialState, action) => {
-    console.log("add review action in reducer", action)
     switch (action.type) {
         case "SET_REVIEWS":
             return action.reviews
         case "ADD_REVIEW":
-            return state.concat(action.review)
+            return state.concat(action.review.data.attributes)
         default:
             return state;
     };
