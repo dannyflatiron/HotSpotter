b689c1edf85a166425ee22e2f3b952bf9cdd162d
dannyflatiron <dannyflatiron@gmail.com>
2020-09-14 03:21:22 -0400

    added case for CLEAR_REVIEWS returns an empty state array for reviews

--- frontend/src/reducers/reviews.js
+++ frontend/src/reducers/reviews.js
@@ -1,11 +1,15 @@
 const initialState = []
 
 export default (state = initialState, action) => {
+    console.log(action)
     switch (action.type) {
         case "SET_REVIEWS":
             return action.reviews
         case "ADD_REVIEW":
             return state.concat(action.review.data.attributes)
+        case "CLEAR_REVIEWS":
+            console.log(state)
+            return []
         default:
             return state;
     };
