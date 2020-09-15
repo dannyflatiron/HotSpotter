05aab1f2c40ecc36cd64c1946f2dd6482637c0f1
dannyflatiron <dannyflatiron@gmail.com>
2020-09-15 01:06:49 -0400

    removed unused imports and commented code. Map component carries several props

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import '../App.css';
 import { connect } from 'react-redux'
 import { Map, Marker, GoogleApiWrapper, InfoWindow } from 'google-maps-react'
-// import { GoogleMap, Marker, useLoadScript, InfoWindow } from '@react-google-maps/api'
-// import { formRelative } from "data-fns"
 import { getCurrentUser } from "../actions/users/currentUser.js"
 import NavBar from "./NavBar.js"
 import Review from "./Review.js"
 import Home from "./Home.js"
 import Login from "./Login.js"
 import Signup from "./Signup.js"
-// import WrappedContainer from "./WrappedContainer.js"
 import NewReview from "./NewReviewForm.js"
-// import { BrowserRouter, Route } from 'react-router-dom';
-import { Router, Route, Link } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import history from '../history';
 import mapStyles from "../mapStyles.js"
 
@@ -26,10 +22,9 @@ class App extends React.Component {
 
   render() {
     const loggedIn = this.props.loggedIn
-    const mapContainerStyle = {
-      // width: "100vw",
-      // height: "100vh"
-      mapStyles
+    const containerStyle = {
+      width: "100%",
+      height: "100vh"
     }
     const center = {
       lat: 40.678177,
@@ -42,21 +37,18 @@ class App extends React.Component {
     const styles = require('../mapStyles.js')
     return (
     <div className="App">
-      {/* <Map google={this.props.google} /> */}
-      {/* <BrowserRouter > this is the right code */}
+      <h1>
+        HotSpotter <span>📶 </span>
+      </h1>
+      <Map google={this.props.google} zoom={12} mapContainerStyle={containerStyle} initialCenter={center} styles={mapStyles} disableDefaultUI={options}/>
       <Router history={history}>
-      {/* { this.props.loggedIn ? <NavBar /> : <Home/> } */}
-        {/* <Route exact path='/' component={() => this.props.loggedIn ? <NavBar/> : <Home/>}/> */}
         { loggedIn ? <NavBar /> : <Home/> } 
         <Route  path='/login' exact component={Login}/>
         <Route  path='/signup' exact component={Signup}/>
         <Route  path='/reviews' exact component={Review}/>
         <Route  path='/reviews/new' exact component={NewReview}/>
-        <Map google={this.props.google} zoom={12} initialCenter={center} styles={mapStyles} disableDefaultUI={options}/>
         <Route  path='/navbar' exact component={NavBar}/>
-
       </Router>
-      {/* </BrowserRouter> */}
     </div>  
 
 
