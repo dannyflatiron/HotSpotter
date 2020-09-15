d047a767d86f7f471cfae68d51445deb8f4918ad
dannyflatiron <dannyflatiron@gmail.com>
2020-09-14 22:46:47 -0400

    added style to map and disabled default UI

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import '../App.css';
 import { connect } from 'react-redux'
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
+import { Map, Marker, GoogleApiWrapper, InfoWindow } from 'google-maps-react'
+// import { GoogleMap, Marker, useLoadScript, InfoWindow } from '@react-google-maps/api'
+// import { formRelative } from "data-fns"
 import { getCurrentUser } from "../actions/users/currentUser.js"
 import NavBar from "./NavBar.js"
 import Review from "./Review.js"
@@ -13,6 +15,7 @@ import NewReview from "./NewReviewForm.js"
 // import { BrowserRouter, Route } from 'react-router-dom';
 import { Router, Route, Link } from 'react-router-dom';
 import history from '../history';
+import mapStyles from "../mapStyles.js"
 
 
 class App extends React.Component {
@@ -23,6 +26,20 @@ class App extends React.Component {
 
   render() {
     const loggedIn = this.props.loggedIn
+    const mapContainerStyle = {
+      // width: "100vw",
+      // height: "100vh"
+      mapStyles
+    }
+    const center = {
+      lat: 40.678177,
+      lng: -73.944160
+    }
+    const options = {
+      disableDefaultUI: true
+    }
+
+    const styles = require('../mapStyles.js')
     return (
     <div className="App">
       {/* <Map google={this.props.google} /> */}
@@ -35,12 +52,13 @@ class App extends React.Component {
         <Route  path='/signup' exact component={Signup}/>
         <Route  path='/reviews' exact component={Review}/>
         <Route  path='/reviews/new' exact component={NewReview}/>
-        <Map google={this.props.google} />
+        <Map google={this.props.google} zoom={12} initialCenter={center} styles={mapStyles} disableDefaultUI={options}/>
         <Route  path='/navbar' exact component={NavBar}/>
 
       </Router>
       {/* </BrowserRouter> */}
     </div>  
+
 
     )
   }
@@ -53,14 +71,7 @@ class App extends React.Component {
 }
 
 const WrappedContainer = GoogleApiWrapper({
-  // apiKey: (process.env.REACT_APP_API_KEY)
+  apiKey: (process.env.REACT_APP_API_KEY)
 })(App)
 
-export default connect(mapStateToProps, { getCurrentUser })(WrappedContainer)
-
-// const WrappedContainer = GoogleApiWrapper({
-//   // apiKey: (process.env.REACT_APP_API_KEY)
-// })(App)
-
-// export default connect(mapStateToProps, { getCurrentUser })(App)
- 
+export default connect(mapStateToProps, { getCurrentUser })(WrappedContainer) 
