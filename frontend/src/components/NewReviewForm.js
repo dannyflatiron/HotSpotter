56738ee275d54b01c3e758003712bcbb40294d23
varit05 <varitpatel@gmail.com>
2020-09-17 20:48:49 +0530

    render infowindow

--- frontend/src/components/NewReviewForm.js
+++ frontend/src/components/NewReviewForm.js
@@ -1,45 +1,59 @@
-import React from 'react';
-import { updateNewReviewForm } from '../actions/reviews/newReviewForm'
-import { resetNewReviewForm } from '../actions/reviews/newReviewForm'
-import { createReview } from '../actions/reviews/createReview.js'
-import { connect } from 'react-redux'
+import React from "react";
+import { updateNewReviewForm } from "../actions/reviews/newReviewForm";
+import { resetNewReviewForm } from "../actions/reviews/newReviewForm";
+import { createReview } from "../actions/reviews/createReview.js";
+import { connect } from "react-redux";
 
-const NewReviewForm = ({newReviewFormData, updateNewReviewForm, createReview, userId, resetNewReviewForm}) => {
-
-  const handleChange = event => {
-    let content  = event.target.value
+const NewReviewForm = ({
+  newReviewFormData,
+  updateNewReviewForm,
+  createReview,
+  userId,
+  resetNewReviewForm,
+}) => {
+  const handleChange = (event) => {
+    let content = event.target.value;
 
     const formData = {
       content,
-      userId
-    }
+      userId,
+    };
 
-    updateNewReviewForm(formData)
-  }
+    updateNewReviewForm(formData);
+  };
 
   const handleSubmit = (event, userId) => {
     // userId might not be needed since the state of form carries the userId already
-    event.preventDefault()
-    createReview(newReviewFormData, userId)
-    resetNewReviewForm()
+    event.preventDefault();
+    createReview(newReviewFormData, userId);
+    resetNewReviewForm();
+  };
 
-  }
-
-  return (
-    
+  return userId ? (
     <form onSubmit={handleSubmit}>
-      <input className="content" name="content" onChange={handleChange} value={newReviewFormData.content} />
-      <input className="submit" type="submit" value="Submit Review"/>
+      <input
+        className='content reviewInput'
+        name='content'
+        onChange={handleChange}
+        value={newReviewFormData.content}
+      />
+      <input className='submit' type='submit' value='Submit Review' />
     </form>
-  )
+  ) : (
+    "Please login or signup to review"
+  );
 };
 
 const mapStateToProps = (state) => {
-  const userId = state.currentUser ? state.currentUser.id: ""
+  const userId = state.currentUser ? state.currentUser.id : "";
   return {
     newReviewFormData: state.newReviewForm,
-    userId
-  } 
-}
+    userId,
+  };
+};
 
-export default connect(mapStateToProps, { updateNewReviewForm, createReview, resetNewReviewForm })(NewReviewForm);
+export default connect(mapStateToProps, {
+  updateNewReviewForm,
+  createReview,
+  resetNewReviewForm,
+})(NewReviewForm);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.js';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import currentUser from './reducers/currentUser'
-import loginForm from './reducers/loginForm'
-import reviews from './reducers/reviews'
-import signupForm from './reducers/signupForm'
-import newReviewForm from './reducers/newReviewForm.js'
-import locations from './reducers/locations.js'
-import locationMarker from './reducers/locationMarker.js'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import currentUser from "./reducers/currentUser";
+import loginForm from "./reducers/loginForm";
+import reviews from "./reducers/reviews";
+import signupForm from "./reducers/signupForm";
+import newReviewForm from "./reducers/newReviewForm.js";
+import locations from "./reducers/locations.js";
+import locationMarker from "./reducers/locationMarker.js";
 
 const reducer = combineReducers({
-    currentUser,
-    loginForm,
-    reviews,
-    signupForm,
-    newReviewForm,
-    locations,
-    locationMarker
-})
+  currentUser,
+  loginForm,
+  reviews,
+  signupForm,
+  newReviewForm,
+  locations,
+  locationMarker,
+});
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
-
-ReactDOM.render(
-<Provider store={store}>
-    <App />
-</Provider>,
-document.getElementById('root')
+export const store = createStore(
+  reducer,
+  composeEnhancer(applyMiddleware(thunk))
 );
 
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
