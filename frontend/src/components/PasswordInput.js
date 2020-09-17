62fc04d914ff044aa0c58d7164e196587cc2be52
dannyflatiron <dannyflatiron@gmail.com>
2020-09-17 12:28:55 -0400

    changed className to match CSS stylgin

--- frontend/src/components/PasswordInput.js
+++ frontend/src/components/PasswordInput.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PasswordInput = (props) => {
     return (
-        <input className="log" type={props.type} placeholder={props.placeholder} value={props.value} name={props.name} onChange={props.onChange}/>
+        <input className="password" type={props.type} placeholder={props.placeholder} value={props.value} name={props.name} onChange={props.onChange}/>
     )
 }
 
--- frontend/src/components/UsernameInput.js
+++ frontend/src/components/UsernameInput.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UsernameInput = (props) => {
     return (
-        < input className="log" type={props.type} placeholder={props.placeholder} value={props.value} name={props.name} onChange={props.onChange}/>
+        < input className="username" type={props.type} placeholder={props.placeholder} value={props.value} name={props.name} onChange={props.onChange}/>
     )
 }
 
