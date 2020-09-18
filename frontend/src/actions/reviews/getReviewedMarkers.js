719b49c5ae54c010c4d21e252af40dbebcc9ac38
dannyflatiron <dannyflatiron@gmail.com>
2020-09-18 17:42:50 -0400

    new action creator that takes data from getMarker. Data compromises of a marker that matches current activated marker in browser

--- frontend/src/actions/reviews/getReviewedMarkers.js
+++ frontend/src/actions/reviews/getReviewedMarkers.js
@@ -5,9 +5,18 @@ export const setReviewedMarkers = (reviews) => {
   };
 };
 
+export const setReviewedMarker = (review) => {
+    // THIS WORKS LEAVE IT ALONE
+  return {
+    type: "SET_REVIEWED_MARKER",
+    review,
+  };
+};
+
 export const getReviewedMarkers = () => {
+  // THIS WORKS LEAVE IT ALONE
   return (dispatch) => {
-    return fetch("http://localhost:3000/api//v1/locations", {
+    return fetch(`http://localhost:3000/api//v1/locations/`, {
       credentials: "include",
       method: "GET",
       headers: {
@@ -19,8 +28,29 @@ export const getReviewedMarkers = () => {
         // if (response.error) {
         //     alert(response.error)
         // } else {
-        dispatch(setReviewedMarkers(response.data));
+          const reviews = response.data.map(d => d.attributes)
+          // console.log("getReviewMarkers, review map:", review)
+        dispatch(setReviewedMarkers(reviews));
         // }
+      })
+      .catch(console.log);
+  };
+};
+
+export const getMarker = (id) => {
+  // THIS WORKS LEAVE IT ALONE
+  return (dispatch) => {
+    return fetch(`http://localhost:3000/api//v1/locations/${id}/`, {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        const t = response.data.attributes
+          dispatch(setReviewedMarker(t))
       })
       .catch(console.log);
   };
