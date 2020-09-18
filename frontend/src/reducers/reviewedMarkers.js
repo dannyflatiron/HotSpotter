a9329666c03b56e3ffc7e89000e88dddfd30db9d
dannyflatiron <dannyflatiron@gmail.com>
2020-09-18 17:47:44 -0400

    reducer ready to send data to frontend

--- frontend/src/reducers/reviewedMarkers.js
+++ frontend/src/reducers/reviewedMarkers.js
@@ -1,9 +1,70 @@
+// const initialState = {
+//     locationReviews: {
+//         id: 1,
+//         location: "8 THOMAS S BOYLAND STREET",
+//         name: "Saratoga - Brooklyn Public Library",
+//         ssid: "BPLUNWIRED",
+//         price: "Free",
+//         reviews: [
+//           {
+//             id: 1,
+//             content: "Please work I hope you do ",
+//             user_id: 1,
+//             location_id: 1,
+//             created_at: "2020-09-18T01:21:49.805Z",
+//             updated_at: "2020-09-18T01:21:49.805Z",
+//           },
+//           {
+//             id: 2,
+//             content: "This is a second test",
+//             user_id: 1,
+//             location_id: 1,
+//             created_at: "2020-09-18T01:22:18.001Z",
+//             updated_at: "2020-09-18T01:22:18.001Z",
+//           },
+//           {
+//             id: 3,
+//             content: "Please work I hope you do ",
+//             user_id: 1,
+//             location_id: 1,
+//             created_at: "2020-09-18T01:21:49.805Z",
+//             updated_at: "2020-09-18T01:21:49.805Z",
+//           },
+//           {
+//             id: 4,
+//             content: "Please work I hope you do ",
+//             user_id: 1,
+//             location_id: 1,
+//             created_at: "2020-09-18T01:21:49.805Z",
+//             updated_at: "2020-09-18T01:21:49.805Z",
+//           },
+//           {
+//             id: 6,
+//             content: "Please work I hope you do ",
+//             user_id: 1,
+//             location_id: 1,
+//             created_at: "2020-09-18T01:21:49.805Z",
+//             updated_at: "2020-09-18T01:21:49.805Z",
+//           },
+//           {
+//             id: 7,
+//             content: "Please work I hope you do ",
+//             user_id: 1,
+//             location_id: 1,
+//             created_at: "2020-09-18T01:21:49.805Z",
+//             updated_at: "2020-09-18T01:21:49.805Z",
+//           },
+//         ],
+//       },
+// }
+
 const initialState = null
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case "SET_REVIEWED_MARKERS":
-            return action.reviews
+          // THIS WORKS LEAVE IT ALONE
+            return action.reviews;
         default: 
             return state;
     };
