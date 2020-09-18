df96fd6dbbe8116446cfe6d0647eb08d31c00eb9
dannyflatiron <dannyflatiron@gmail.com>
2020-09-17 20:00:48 -0400

    create endpoints for POST new locaiton+review data and endpoint to make data accessible

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   post "/api/v1/login", to: "api/v1/sessions#create"
   post "/api/v1/signup", to: "api/v1/users#create"
+  post "/api/v1/signup", to: "api/v1/reviews#create"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
 
 
@@ -23,6 +24,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :reviews
       end
     end
   
