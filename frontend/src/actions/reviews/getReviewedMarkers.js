b77339b4b74240f5aeec9da9e241433b13e7995c
dannyflatiron <dannyflatiron@gmail.com>
2020-09-19 02:46:06 -0400

    retriving error messages from backend if promise returns an error

--- frontend/src/actions/reviews/getReviewedMarkers.js
+++ frontend/src/actions/reviews/getReviewedMarkers.js
@@ -36,13 +36,13 @@ export const getReviewedMarkers = () => {
       .then((response) => response.json())
       .then((response) => {
         console.log("after submit", response)
-        // if (response.error) {
-        //     alert(response.error)
-        // } else {
+        if (response.error) {
+            alert(response.error)
+        } else {
           const reviews = response.data.map(d => d.attributes)
           // console.log("getReviewMarkers, review map:", review)
         dispatch(setReviewedMarkers(reviews));
-        // }
+        }
       })
       .catch(console.log);
   };
@@ -52,7 +52,7 @@ export const getMarker = (id) => {
   // THIS WORKS LEAVE IT ALONE
   console.log("what happens after click ID: ", id)
   return (dispatch) => {
-    return fetch(`http://localhost:3000/api//v1/locations/${id}/`, {
+    return fetch(`http://localhost:3000/api//v1/locations/${id}`, {
       credentials: "include",
       method: "GET",
       headers: {
