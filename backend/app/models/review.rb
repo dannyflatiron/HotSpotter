ae5f429eed04abe0f518504d6b2f3d9836981913
dannyflatiron <dannyflatiron@gmail.com>
2020-09-19 02:52:44 -0400

    added validations for empty content

--- backend/app/models/review.rb
+++ backend/app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :location
 
+  validates :content, :presence => true
   validates :content, :presence => true, :length => { :minimum => 10 }
 end
