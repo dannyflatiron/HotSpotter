bd897f8abaae1224cd720000847b38dde3ab92f5
dannyflatiron <dannyflatiron@gmail.com>
2020-09-19 06:09:22 -0400

    dropped database to test out frontend

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_222551) do
+ActiveRecord::Schema.define(version: 2020_09_19_075328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_222551) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "object_id"
   end
 
   create_table "reviews", force: :cascade do |t|
