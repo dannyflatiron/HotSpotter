1a0e43be0e17994625e8b017aa9bc461f4292373
dannyflatiron <dannyflatiron@gmail.com>
2020-10-05 03:15:15 -0400

    feat : using NavLink to access activeStyle prop and removed textDecoration. For better styling

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 
 const Home = () => (
   <div className="home" style={{zIndex:10}}>
     <span className="log" style={{zIndex:10}}>
-      <Link className="log" style={{zIndex:10}} to="/signup">Sign Up</Link> or <Link className="log" style={{zIndex:10}} to="/login">Log In</Link>
+      <NavLink className="log" activeStyle={{ color: 'red' }} style={{zIndex:10}, {textDecoration: 'none'}} to="/signup">Sign Up</NavLink> | <NavLink className="log" activeStyle={{ color: 'red' }} style={{zIndex:10}, {textDecoration: 'none'}} to="/login">Log In</NavLink>
     </span>
   </div>
 
